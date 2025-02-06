4ae418c06f9de1a1ee18c76720703c31cb0d03e3
zhouboyi <zby02126265@alibaba-inc.com>
2025-02-06 11:55:51 +0800

    feat(command): 完善命令相关功能

--- src/controller/command.go
+++ src/controller/command.go
@@ -6,8 +6,65 @@ import (
 	"note-echo/src/repository"
 )
 
+// One 使用 ObjectId 查询单条命令
+func One(ctx echo.Context) error {
+	command := repository.One(ctx)
+	return ctx.JSON(http.StatusOK, command)
+}
+
+// OneByName 使用命令名称查询单条命令
+func OneByName(ctx echo.Context) error {
+	command := repository.OneByName(ctx)
+	return ctx.JSON(http.StatusOK, command)
+}
+
 // List 查询所有命令
 func List(ctx echo.Context) error {
 	commandArray := repository.List(ctx)
 	return ctx.JSON(http.StatusOK, commandArray)
+}
+
+// ListName 查询所有命令的名称
+func ListName(ctx echo.Context) error {
+	nameArray := repository.ListName(ctx)
+	return ctx.JSON(http.StatusOK, nameArray)
+}
+
+// InsertOne 新增单条命令
+func InsertOne(ctx echo.Context) error {
+	result, commandName := repository.InsertOne(ctx)
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"result":  result,
+		"command": commandName,
+	})
+}
+
+// InsertMany 新增多条命令
+func InsertMany(ctx echo.Context) error {
+	result := repository.InsertMany(ctx)
+	return ctx.JSON(http.StatusOK, result)
+}
+
+// UpdateOne 更新单条命令
+func UpdateOne(ctx echo.Context) error {
+	result := repository.UpdateOne(ctx)
+	return ctx.JSON(http.StatusOK, result)
+}
+
+// DeleteOne 删除单条命令
+func DeleteOne(ctx echo.Context) error {
+	result, objectId := repository.DeleteOne(ctx)
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"result": result,
+		"_id":    objectId,
+	})
+}
+
+// DeleteMany 删除多条命令
+func DeleteMany(ctx echo.Context) error {
+	result, objectIds := repository.DeleteMany(ctx)
+	return ctx.JSON(http.StatusOK, map[string]interface{}{
+		"result": result,
+		"_ids":   objectIds,
+	})
 }
--- src/repository/command.go
+++ src/repository/command.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -31,6 +32,58 @@ func Collection(ctx echo.Context) *mongo.Collection {
 	return collection
 }
 
+// One 使用 ObjectId 查询单条命令
+func One(ctx echo.Context) model.Command {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 获取命令 Id
+	commandId := ctx.Param("commandId")
+	// 将命令 Id 转换为 ObjectId 格式
+	objectId, errHex := primitive.ObjectIDFromHex(commandId)
+	if errHex != nil {
+		log.Println(errHex)
+	}
+
+	// 使用 ObjectId 查询数据
+	result := collection.FindOne(ctx.Request().Context(), bson.M{
+		"_id": objectId,
+	})
+
+	// 命令结构体对象
+	var command model.Command
+	// 将查询结果赋值给命令结构体对象
+	err := result.Decode(&command)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return command
+}
+
+// OneByName 使用命令名称查询单条命令
+func OneByName(ctx echo.Context) model.Command {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 获取命令名称
+	commandName := ctx.Param("commandName")
+	// 使用命令名称查询数据
+	result := collection.FindOne(ctx.Request().Context(), bson.M{
+		"command": commandName,
+	})
+
+	// 命令结构体对象
+	var command model.Command
+	// 将查询结果赋值给命令结构体对象
+	err := result.Decode(&command)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return command
+}
+
 // List 查询所有命令
 func List(ctx echo.Context) []model.Command {
 	// 获取集合连接
@@ -55,4 +108,153 @@ func List(ctx echo.Context) []model.Command {
 	}
 
 	return commandArray
+}
+
+// ListName 查询所有命令的名称
+func ListName(ctx echo.Context) []string {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 查询所有命令
+	cursor, err := collection.Find(ctx.Request().Context(), bson.D{})
+	if err != nil {
+		log.Println(err)
+	}
+
+	// 命令名称数组
+	var nameArray []string
+	// 使用 cursor 指针遍历获取数据
+	for cursor.Next(ctx.Request().Context()) {
+		command := model.Command{}
+		err := cursor.Decode(&command)
+		if err != nil {
+			log.Println(err)
+		}
+		nameArray = append(nameArray, command.Command)
+	}
+
+	return nameArray
+}
+
+// InsertOne 新增单条命令
+func InsertOne(ctx echo.Context) (*mongo.InsertOneResult, string) {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 命令结构体对象
+	var command model.Command
+	// 将请求体参数赋值给命令结构体对象
+	errBind := ctx.Bind(&command)
+	if errBind != nil {
+		log.Println(errBind)
+	}
+	// 生成 ObjectID
+	command.Id = primitive.NewObjectID()
+
+	// 新增单条命令
+	result, err := collection.InsertOne(ctx.Request().Context(), command)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return result, command.Command
+}
+
+// InsertMany 新增多条命令
+func InsertMany(ctx echo.Context) *mongo.InsertManyResult {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// interface 数组
+	var commandList []interface{}
+	// 将请求体参数赋值给 interface 数组
+	errBind := ctx.Bind(&commandList)
+	if errBind != nil {
+		log.Println(errBind)
+	}
+
+	// 新增多条命令
+	result, err := collection.InsertMany(ctx.Request().Context(), commandList)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return result
+}
+
+// UpdateOne 更新单条命令
+func UpdateOne(ctx echo.Context) *mongo.UpdateResult {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 命令结构体对象
+	var command model.Command
+	// 将请求体参数赋值给命令结构体对象
+	errBind := ctx.Bind(&command)
+	if errBind != nil {
+		log.Println(errBind)
+	}
+
+	// 根据 ObjectId 更新单条命令
+	result, err := collection.UpdateOne(ctx.Request().Context(), bson.M{"_id": command.Id}, bson.M{"$set": command})
+	if err != nil {
+		log.Println(err)
+	}
+
+	return result
+}
+
+// DeleteOne 删除单条命令
+func DeleteOne(ctx echo.Context) (*mongo.DeleteResult, primitive.ObjectID) {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 获取命令 Id
+	commandId := ctx.Param("commandId")
+	// 将命令 Id 转换为 ObjectId 格式
+	objectId, errHex := primitive.ObjectIDFromHex(commandId)
+	if errHex != nil {
+		log.Println(errHex)
+	}
+
+	// 根据 ObjectId 删除单条命令
+	result, err := collection.DeleteOne(ctx.Request().Context(), bson.M{"_id": objectId})
+	if err != nil {
+		log.Println(err)
+	}
+
+	return result, objectId
+}
+
+// DeleteMany 删除多条命令
+func DeleteMany(ctx echo.Context) (*mongo.DeleteResult, []primitive.ObjectID) {
+	// 获取集合连接
+	collection := Collection(ctx)
+
+	// 命令 Id 数组
+	var commandIds []string
+	// 将请求体参数赋值给命令 Id 数组
+	errBind := ctx.Bind(&commandIds)
+	if errBind != nil {
+		log.Println(errBind)
+	}
+
+	// ObjectId 数组
+	var objectIds []primitive.ObjectID
+	// 遍历命令 Id 数组, 转换为 ObjectId 格式
+	for _, commandId := range commandIds {
+		objectId, errHex := primitive.ObjectIDFromHex(commandId)
+		if errHex != nil {
+			log.Println(errHex)
+		}
+		objectIds = append(objectIds, objectId)
+	}
+
+	// 根据 ObjectId 数组删除多条命令
+	result, err := collection.DeleteMany(ctx.Request().Context(), bson.M{"_id": bson.M{"$in": objectIds}})
+	if err != nil {
+		log.Println(err)
+	}
+
+	return result, objectIds
 }
